0871e812da653a4fdef1572b0f77e32a83907803
Dominique kambale <dominique.kambale-baraka@epitech.eu>
2024-11-14 16:49:00 +0100

    new version

--- .nuxt/types/schema.d.ts
+++ .nuxt/types/schema.d.ts
@@ -2,18 +2,6 @@ import { NuxtModule, RuntimeConfig } from '@nuxt/schema'
 declare module '@nuxt/schema' {
   interface NuxtOptions {
     /**
-     * Configuration for `@nuxtjs/tailwindcss`
-     */
-    ["tailwindcss"]: typeof import("@nuxtjs/tailwindcss").default extends NuxtModule<infer O> ? O : Record<string, any>
-    /**
-     * Configuration for `@pinia/nuxt`
-     */
-    ["pinia"]: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? O : Record<string, any>
-    /**
-     * Configuration for `@nuxtjs/supabase`
-     */
-    ["supabase"]: typeof import("@nuxtjs/supabase").default extends NuxtModule<infer O> ? O : Record<string, any>
-    /**
      * Configuration for `@nuxt/devtools`
      */
     ["devtools"]: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? O : Record<string, any>
@@ -24,18 +12,6 @@ declare module '@nuxt/schema' {
   }
   interface NuxtConfig {
     /**
-     * Configuration for `@nuxtjs/tailwindcss`
-     */
-    ["tailwindcss"]?: typeof import("@nuxtjs/tailwindcss").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    /**
-     * Configuration for `@pinia/nuxt`
-     */
-    ["pinia"]?: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    /**
-     * Configuration for `@nuxtjs/supabase`
-     */
-    ["supabase"]?: typeof import("@nuxtjs/supabase").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    /**
      * Configuration for `@nuxt/devtools`
      */
     ["devtools"]?: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
@@ -43,27 +19,12 @@ declare module '@nuxt/schema' {
      * Configuration for `@nuxt/telemetry`
      */
     ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxtjs/tailwindcss", Exclude<NuxtConfig["tailwindcss"], boolean>] | ["@pinia/nuxt", Exclude<NuxtConfig["pinia"], boolean>] | ["@nuxtjs/supabase", Exclude<NuxtConfig["supabase"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
   }
 }
 declare module 'nuxt/schema' {
   interface NuxtOptions {
     /**
-     * Configuration for `@nuxtjs/tailwindcss`
-     * @see https://www.npmjs.com/package/@nuxtjs/tailwindcss
-     */
-    ["tailwindcss"]: typeof import("@nuxtjs/tailwindcss").default extends NuxtModule<infer O> ? O : Record<string, any>
-    /**
-     * Configuration for `@pinia/nuxt`
-     * @see https://www.npmjs.com/package/@pinia/nuxt
-     */
-    ["pinia"]: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? O : Record<string, any>
-    /**
-     * Configuration for `@nuxtjs/supabase`
-     * @see https://www.npmjs.com/package/@nuxtjs/supabase
-     */
-    ["supabase"]: typeof import("@nuxtjs/supabase").default extends NuxtModule<infer O> ? O : Record<string, any>
-    /**
      * Configuration for `@nuxt/devtools`
      * @see https://www.npmjs.com/package/@nuxt/devtools
      */
@@ -76,21 +37,6 @@ declare module 'nuxt/schema' {
   }
   interface NuxtConfig {
     /**
-     * Configuration for `@nuxtjs/tailwindcss`
-     * @see https://www.npmjs.com/package/@nuxtjs/tailwindcss
-     */
-    ["tailwindcss"]?: typeof import("@nuxtjs/tailwindcss").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    /**
-     * Configuration for `@pinia/nuxt`
-     * @see https://www.npmjs.com/package/@pinia/nuxt
-     */
-    ["pinia"]?: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    /**
-     * Configuration for `@nuxtjs/supabase`
-     * @see https://www.npmjs.com/package/@nuxtjs/supabase
-     */
-    ["supabase"]?: typeof import("@nuxtjs/supabase").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    /**
      * Configuration for `@nuxt/devtools`
      * @see https://www.npmjs.com/package/@nuxt/devtools
      */
@@ -100,7 +46,7 @@ declare module 'nuxt/schema' {
      * @see https://www.npmjs.com/package/@nuxt/telemetry
      */
     ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxtjs/tailwindcss", Exclude<NuxtConfig["tailwindcss"], boolean>] | ["@pinia/nuxt", Exclude<NuxtConfig["pinia"], boolean>] | ["@nuxtjs/supabase", Exclude<NuxtConfig["supabase"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
   }
   interface RuntimeConfig {
    app: {
@@ -116,45 +62,9 @@ declare module 'nuxt/schema' {
    nitro: {
       envPrefix: string,
    },
-
-   supabase: {
-      serviceKey: any,
-   },
   }
   interface PublicRuntimeConfig {
-   SUPABASE_URL: string,
 
-   SUPABASE_KEY: string,
-
-   supabase: {
-      url: string,
-
-      key: string,
-
-      redirect: boolean,
-
-      redirectOptions: {
-         login: string,
-
-         callback: string,
-
-         exclude: Array<any>,
-
-         cookieRedirect: boolean,
-      },
-
-      cookieName: string,
-
-      cookieOptions: {
-         maxAge: number,
-
-         sameSite: string,
-
-         secure: boolean,
-      },
-
-      clientOptions: any,
-   },
   }
 }
 declare module 'vue' {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['@/assets/css/tailwind.css'],
-  
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt' , '@nuxtjs/supabase'],
-  runtimeConfig: {
-    public: {
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_KEY: process.env.SUPABASE_KEY,
-    }
-  },
-  compatibilityDate: '2024-10-18',
-  ssr: false,
-  components: true,
-});
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
